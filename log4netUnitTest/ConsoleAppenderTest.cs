4be83541c8eeabcc0ab3243079e4be71cef04a25
dylanhouston7 <dylanhouston7@gmail.com>
2016-07-13 15:39:44 -0400

    Unit test project checkin

---
+++ log4netUnitTest/ConsoleAppenderTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace log4netUnitTest
+{
+    [TestClass]
+    public class ConsoleAppenderTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
